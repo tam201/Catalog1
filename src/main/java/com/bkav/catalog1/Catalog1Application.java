aff609ae18b4a9d01a6a127149dc65085f57ed8c
TamHTc <TamHTc@bkav.com>
2021-12-10 09:52:52 +0700

    Initial commit

---
+++ src/main/java/com/bkav/catalog1/Catalog1Application.java
@@ -0,0 +1,13 @@
+package com.bkav.catalog1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Catalog1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Catalog1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bkav/catalog1/Catalog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkav.catalog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Catalog1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
